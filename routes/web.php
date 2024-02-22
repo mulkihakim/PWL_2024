8a6298393d59ada19045947aa911ba3d5c18cf79
mulkihakim <mulkihkm@gmail.com>
2024-02-22 16:15:52 +0700

    belajar routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,36 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Basic Routing
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return '2241720131<br>Mulki Hakim';
+});
+
+// Route Parameters
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function 
+($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($halamanid) {
+    return 'Halaman Artikel dengan ID ' . $halamanid;
+});
+
+// Optional Parameters
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
 });
